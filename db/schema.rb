654e68b12499dddec52d532a8fee0668515cd9cb
almarrim <mosalmarri@gmail.com>
2019-11-25 13:05:05 +0300

    delete reservation model and related file and keep restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 2019_11_25_084911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "reservations", force: :cascade do |t|
-    t.datetime "time"
-    t.bigint "user_id"
-    t.bigint "restaurant_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["restaurant_id"], name: "index_reservations_on_restaurant_id"
-    t.index ["user_id"], name: "index_reservations_on_user_id"
-  end
-
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "img"
@@ -48,7 +38,5 @@ ActiveRecord::Schema.define(version: 2019_11_25_084911) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "reservations", "restaurants"
-  add_foreign_key "reservations", "users"
   add_foreign_key "restaurants", "users"
 end
